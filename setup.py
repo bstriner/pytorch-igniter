5f1dfe27746a9cd8f723e98c4527b01bd01a0310
Benjamin Striner <bstriner@gmail.com>
2020-09-18 02:28:22 -0400

    travis

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(name='pytorch-igniter',
-      version='0.0.9',
+      version='0.0.10',
       author='Ben Striner',
       url='https://github.com/bstriner/pytorch-igniter',
       install_requires=[
