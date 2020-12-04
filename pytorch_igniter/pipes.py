6a99184e9992a31f3cbb46398c8797788cd5117f
Benjamin Striner <bstriner@gmail.com>
2020-12-04 06:38:33 -0500

    pipes

--- pytorch_igniter/pipes.py
+++ pytorch_igniter/pipes.py
@@ -1,5 +1,5 @@
 from torch.utils.data import IterableDataset, get_worker_info
-from aws_sagemaker_remote.util.pipes import chunk_iterable
+from aws_sagemaker_remote.util.pipes import chunk_iterable, PipeIterator
 from sagemaker.amazon.common import read_recordio
 import os
 from torch.multiprocessing import Value, Lock
@@ -18,21 +18,23 @@ class PipeDataset(IterableDataset):
     #    self.count = count
     #    self.lock = lock
 
-    def transform_fn(self, rec, worker):
+    def transform(self, rec, worker):
+        if self.transform_fn:
+            rec = self.transform_fn(rec, worker)
         return rec
 
-    def __init__(self, paths, size, transform=None, buffering=0):
+    def __init__(self, paths, size, transform_fn=None, epochs=1):
         if isinstance(paths, dict):
             paths = list(paths.values())
         elif isinstance(paths, str):
             paths = [paths]
         self.paths = paths
-        self.counters = [
-            MultiprocessingCounter() for _ in range(len(paths))
+        self.pipes = [
+            PipeIterator(path, size=size, count=0, epochs=epochs)
+            for path in paths
         ]
         self.size = size
-        self.transform = transform
-        self.buffering = buffering
+        self.transform_fn = transform_fn
         super(PipeDataset, self).__init__()
 
     def __iter__(self):
@@ -43,33 +45,12 @@ class PipeDataset(IterableDataset):
             worker = info.id
         if worker is None:
             assert len(self.paths) == 1
-            path = self.paths[0]
-            counter = self.counters[0]
-            count = counter.local
-            counter.local = count+1
+            worker = 0
         else:
             assert worker >= 0
             assert worker < len(self.paths)
-            path = self.paths[worker]
-            counter = self.counters[worker]
-            with counter.lock:
-                count = counter.value.value
-                counter.value.value = count+1
-        # if path not in self.counts:
-        #    self.counts[path] = 0
-        #if worker:
-        #else:
 
-        pipe_path = "{}_{}".format(path, count)
-        print("Opening pipe on worker [{}]: {}".format(worker, pipe_path))
-        if os.path.isdir(pipe_path):
-            print("Is dir: {}".format(pipe_path))
-        if os.path.isfile(pipe_path):
-            print("Is file: {}".format(pipe_path))
-        with open(pipe_path, 'rb', buffering=self.buffering) as f:
-            for rec in chunk_iterable(read_recordio(f), self.size, last='error'):
-                rec = self.transform_fn(rec, worker)
-                if self.transform:
-                    rec = self.transform(rec, worker)
-                yield rec
-            print("Pipe {} empty".format(pipe_path))
+        pipe = self.pipes[worker]
+        for rec in pipe:
+            rec = self.transform(rec, worker)
+            yield rec
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 with open(os.path.abspath(os.path.join(__file__, '../README.rst')), encoding='utf-8') as f:
     long_description = f.read()
 setup(name='pytorch-igniter',
-      version='0.0.42',
+      version='0.0.44',
       author='Ben Striner',
       author_email="bstriner@gmail.com",
       url='https://github.com/bstriner/pytorch-igniter',
@@ -15,6 +15,14 @@ setup(name='pytorch-igniter',
           'mlflow',
           'aws-sagemaker-remote'
       ],
+      package_data={'': [
+          '**/Dockerfile',
+          '**/*.sh',
+          '**/*.txt',
+          '**/*.yaml',
+          '**/*.bat'
+      ]},
+      include_package_data=True,
       packages=find_packages(),
       long_description=long_description,
       long_description_content_type='text/x-rst')
